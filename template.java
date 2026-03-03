daa46ba40125ee129f9e23c8f06ddada78e9ecce
isjuny0 <isjuny0k@gmail.com>
2026-03-03 23:23:27 +0900

    기본 입출력 템플릿

---
+++ template.java
@@ -0,0 +1,14 @@
+// 기본 입출력 템플릿 — 반드시 외워서 쓸 것
+public class Main {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+
+        StringBuilder sb = new StringBuilder();
+        // 풀이
+        System.out.print(sb);
+    }
+}
