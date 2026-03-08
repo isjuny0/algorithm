b8cbb851b949152b1deca201aa3c60623648fc07
isjuny0 <isjuny0k@gmail.com>
2026-03-08 20:32:33 +0900

    priorityQueue(heap) 11279

---
+++ heap/11279/Main.java
@@ -0,0 +1,27 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
+
+        int N = Integer.parseInt(br.readLine());
+
+        while(N-- > 0) {
+            int x = Integer.parseInt(br.readLine());
+
+            if (x > 0) {
+                pq.add(x);
+            }
+            else if (x == 0) {
+                if (pq.isEmpty()) {
+                    System.out.println(0);
+                } else {
+                    System.out.println(pq.poll());
+                }
+            }
+        }
+    }
+}
