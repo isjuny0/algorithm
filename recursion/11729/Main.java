23c3d13a5f5036b0c1121f4018ca2ac630680adb
isjuny0 <isjuny0k@gmail.com>
2026-03-23 16:32:52 +0900

    백준 11729 리펙토링 - 시간 초과 오류를 stringbuilder를 사용하여 해결

--- recursion/11729/Main.java
+++ recursion/11729/Main.java
@@ -13,20 +13,21 @@ public class Main {
 
         System.out.println((int)Math.pow(2, N) - 1);
         hanoi(1, 3, N);
+        System.out.println(sb.toString());
     }
 
     // 기둥 a, b, 6-a-b / 원판 n개를 a번 기둥에서 b번 기둥으로 옮기는 방법을 출력하는 함수
     private static void hanoi(int a, int b, int n) {
         // base condition
         if (n == 1) {
-            System.out.println(a + " " + b);
+            sb.append(a).append(" ").append(b).append("\n");
             return;
         }
 
         // case1: n-1개의 원판을 기둥 a에서 기둥 6-a-b로 옮긴다.
         hanoi(a, 6-a-b, n-1);
         // case2: n번 원판을 기둥 a에서 기둥 b로 옮긴다.
-        System.out.println(a + " " + b);
+        sb.append(a).append(" ").append(b).append("\n");
         // case3: n-1개의 원판을 기둥 6-a-b에서 기둥 b로 옮긴다.
         hanoi(6-a-b, b, n-1);
     }
